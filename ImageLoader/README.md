c54dd613077b3ea364937c48e1bce20dbbc18bfd
mohammad <mohammad.bakhtiari.ai@gmail.com>
2021-12-02 18:14:30 +0100

    ImageLoader app added

---
+++ ImageLoader/README.md
@@ -0,0 +1,47 @@
+# Image Loader FeatureCloud App
+
+## Description
+Using Image Loader app, users can load their images from different directories and perform some local preprocessing 
+steps without communicating with the coordinator.
+
+## Input
+- path to the directory including images  
+- image formats to be searched for in the directory
+- label_file: labels of images, which can be either in a file inside the directory,
+  or the name of the directory that includes images.
+  The labels file could be in .csv or .txt extension, 
+  in both cases first line should be 'name,label', and the separator of ',' should be used!   
+## Output
+- dataset.npy: including [image sample, labels]
+
+## Preprocessing
+
+- Resize: resizing images to  a specific 'width' and 'height'
+- Crop: croping images based on a specific x and y coordinates, with a particular width and height
+
+## Workflows
+Image loader needs no communication with the coordinator during its run can be optionally used at the begining 
+of a workflow with apps that require images in NumPy files 
+- Post: Various Preprocessing or Analysis apps that support `.npy` format for their input files
+  (e.g. Image normalization, Cross Validation, and/or Deep Learning)
+![Workflow](../data/images/ImageLoader.png)
+## Config
+Following config information should be included in the `config.yml` to run the Image Loader app in a workflow:
+```
+fc_image_loader:
+  local_dataset:
+    ds_dir: ds
+    image_format: jpeg
+    target_value: dir
+    sep: ','
+  image_resize:
+    width: 28
+    height: 28
+  image_crop:
+    x_coordinate: 0
+    y_coordinate: 0
+    width: 28
+    height: 28
+  result:
+    data: dataset.npy
+```
---
+++ ImageLoader/app.py
@@ -0,0 +1,133 @@
+"""
+    FeatureCloud Image Loader Application
+    Copyright 2021 Mohammad Bakhtiari. All Rights Reserved.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+from FeatureCloud.CustomStates import ConfigState
+from FeatureCloud.engine.app import app_state, AppState, Role, LogLevel
+from FeatureCloud.engine.app import State as op_state
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image
+import glob
+
+name = 'fc_image_loader'
+
+default_config = {
+    name: {
+        'local_dataset': {
+            'ds_dir': 'ds',
+            'image_format': 'jpeg',
+            'target_value': 'dir',
+            'sep': ','
+        },
+        'image_resize': {
+            'width': 28,
+            'height': 28
+        },
+        'image_crop': {
+            'x_coordinate': 0,
+            'y_coordinate': 0,
+            'width': 28,
+            'height': 28
+        },
+        'result': {
+            'data': 'dataset.npy'
+        }
+    }
+}
+
+requirements = ['pandas', 'Pillow']
+
+
+@app_state(name='initial', role=Role.BOTH, app_name=name)
+class ImgLoader(ConfigState.State):
+
+    def register(self):
+        self.register_transition('WriteResults', Role.BOTH)
+
+    def run(self):
+        self.lazy_init()
+        self.read_config()
+        self.finalize_config()
+        samples, self.app.internal['labels'] = \
+            self.load_images(ds_dir=f"{self.input_dir}/{self.config['local_dataset']['ds_dir']}")
+        self.update(progress=0.3)
+        self.app.internal['samples'] = self.image_preprocess(samples)
+        self.update(progress=0.8)
+        return 'WriteResults'
+
+    def load_images(self, ds_dir):
+        self.app.log(f"Reading {ds_dir}", LogLevel.DEBUG)
+        self.update(progress=0.1)
+        samples = []
+        labels = []
+        if '.txt' in self.config['local_dataset']['target_value'] or '.csv' in self.config['local_dataset']['target_value']:
+            folders = []
+            for folder in glob.glob(f'{ds_dir}/*/'):
+                folders.append(folder.strip().split('/')[-2])
+            for folder in folders:
+                labels_file = f"{ds_dir}/folder/{self.config['local_dataset']['target_value']}"
+                if os.path.exists(labels_file):
+                    df = pd.read_csv(labels_file, sep=self.config['local_dataset']['sep'])
+                else:
+                    self.update(state=op_state.ERROR)
+                    self.app.log(f"No {self.config['local_dataset']['target_value']} file found in {labels_file}!",
+                                 LogLevel.FATAL)
+                for format in self.config['local_dataset']['image_format']:
+                    for filename in glob.glob(f'{ds_dir}/{folder}/*.{format}'):  # assuming gif
+                        samples.append(Image.open(filename))
+                        labels.append(df[df.name == filename.strip().split('/')[-1]].label.values.item())
+            return samples, labels
+
+        labels_folders = []
+        for folder in glob.glob(f'{ds_dir}/*/'):
+            labels_folders.append(folder.strip().split('/')[-2])
+        for folder in labels_folders:
+            for format in self.config['local_dataset']['image_format']:
+                for filename in glob.glob(f'{ds_dir}/{folder}/*.{format}'):  # assuming gif
+                    samples.append(Image.open(filename))
+                    labels.append(folder)
+        return samples, labels
+
+    def image_preprocess(self, samples):
+        resize, crop = self.config.get('image_resize', False), self.config.get('image_crop', False)
+        if resize:
+            resize_dim = (resize['width'], resize['height'])
+            resized_samples = []
+            for sample in samples:
+                resized_samples.append(sample.resize(resize_dim))
+            samples = resized_samples
+        if crop:
+            crop_sizes = (crop['x_coordinate'],
+                          crop['y_coordinate'],
+                          crop['width'],
+                          crop['height'])
+            cropped_samples = []
+            for sample in samples:
+                cropped_samples.append(sample.crop(crop_sizes))
+            samples = cropped_samples
+        return samples
+
+
+@app_state(name='WriteResults', role=Role.BOTH)
+class WriteResults(AppState):
+    def register(self):
+        self.register_transition('terminal', Role.BOTH)
+
+    def run(self) -> str or None:
+        samples = []
+        for sample in self.app.internal['samples']:
+            samples.append(np.asarray(sample))
+        np.save(self.app.internal['output_files']['data'][0], [samples, self.app.internal['labels']])
+        self.update(progress=0.99)
+        return 'terminal'
