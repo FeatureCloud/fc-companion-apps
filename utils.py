2e38d1d16427bdb8cdad2759931368ca2cb7f4b8
mohammad <mohammad.bakhtiari.ai@gmail.com>
2021-12-09 14:30:04 +0100

    run method added

--- utils.py
+++ utils.py
@@ -1,6 +1,26 @@
 import numpy as np
 import pandas as pd
-from FeatureCloud.engine.app import LogLevel
+from FeatureCloud.engine.app import LogLevel, app
+from FeatureCloud.api.http_ctrl import api_server
+from FeatureCloud.api.http_web import web_server
+from bottle import Bottle
+
+
+def run(host='localhost', port=5000):
+    """ run the docker container on specific host and port.
+
+    Parameters
+    ----------
+    host: str
+    port: int
+
+    """
+
+    app.register()
+    server = Bottle()
+    server.mount('/api', api_server)
+    server.mount('/web', web_server)
+    server.run(host=host, port=port)
 
 
 def save_numpy(file_name, features, labels, target):
